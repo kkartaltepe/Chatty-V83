26cad29c9cff8f28910b0cb146818ba04114c5dd
Kurt Kartaltepe <kkartaltepe@gmail.com>
2011-06-18 13:02:49 -0500

    working base, and imported all those lovely maple encryption and tool files we love so much.

---
+++ src/main/java/net/arcreactor/chatty/Chatty.java
@@ -0,0 +1,19 @@
+package net.arcreactor.chatty;
+
+import org.apache.mina.core.service.IoConnector;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 6/17/11
+ * Time: 11:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chatty {
+
+    public Chatty(){
+//        IoConnector connector = new NioSocketConnector();
+//        connector.getFilterChain().addLast("AES", new);
+    }
+}
---
+++ src/main/java/net/arcreactor/chatty/ChattyMain.java
@@ -0,0 +1,15 @@
+package net.arcreactor.chatty;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 6/17/11
+ * Time: 11:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChattyMain {
+    public static void main(String args[]){
+        new Chatty();
+    }
+}
